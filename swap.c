97981ef52f4f4c8c0ebccec6f6447edc53b5a4bf
Andrew Druzhinsky <adruzhinsky@pulsepoint.com>
2018-07-23 17:19:38 -0400

    Initial commit
    amend file

---
+++ swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 34;
+    int y = 50;
+	int z = 999;
+    
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+    
+    // swapping...
+    int temp = x;
+    x = y;
+    y = temp;
+    
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
