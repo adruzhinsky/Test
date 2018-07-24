4277d015bb5f7140d93e750d52e5bb5969a582c4
Andrew Druzhinsky <adruzhinsky@pulsepoint.com>
2018-07-24 13:25:36 -0400

    test

--- swap.c
+++ swap.c
@@ -6,10 +6,7 @@ int main(void)
     int y = 50;
     int z = 888;
     int z = 999;
-    int z = 111111;
-    int z = 2222222;
-    int z = 77777;
-    int z = 12357;	
+	int z = 3333333333;
     printf("x is %i\n", x);
     printf("y is %i\n", y);
     
