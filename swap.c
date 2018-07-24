2222a2a01c58072b5a964f220a8d44567df69ec2
adruzhinsky <41093867+adruzhinsky@users.noreply.github.com>
2018-07-24 13:21:23 -0400

    Update swap.c

--- swap.c
+++ swap.c
@@ -8,8 +8,8 @@ int main(void)
     int z = 999;
     int z = 111111;
     int z = 2222222;
-
-	  int z = 77777;
+    int z = 77777;
+    int z = 12357;	
     printf("x is %i\n", x);
     printf("y is %i\n", y);
     
